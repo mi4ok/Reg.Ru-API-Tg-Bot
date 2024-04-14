ba59e256feb8c45a10096c351f15c6bb44190473
mikhail.tyzhnev <mikhail.tyzhnev@simpleone.ru>
2024-04-14 19:21:23 +0300

    fix

--- index.php
+++ index.php
@@ -69,11 +69,11 @@ try {
 
                 $keyboard = new InlineKeyboardMarkup([
                     [
-                        ['text' => "Все сервера", 'callback_data' => 'all_servers']
+                        ['text' => "✅ Да", 'callback_data' => "confirm_reload_server_$serverId"],
+                        ['text' => "❌ Нет", 'callback_data' => "cancel_delete_server_$serverId"],
                     ]
                 ]);
-                $bot->sendMessage($chat_id, 'Начинаем перезагружать сервер: ' . $serverName, null, false, null, $keyboard);
-                //handleServerReboot($serverId, TOKEN_REG_RU, URL);
+                $bot->editMessageText($chat_id, $message->getMessageId(), 'Точно хотите перезагрузить сервер? ' . PHP_EOL . $serverName . PHP_EOL . 'ID: ' . $serverId, null, false, $keyboard);
                 break;
             case (bool)preg_match('/^delete_server_(\d+)$/', $callback_data, $matches):
                 $serverId = $matches[1];
@@ -88,11 +88,11 @@ try {
                 $keyboard = new InlineKeyboardMarkup([
                     [
                         ['text' => "✅ Да", 'callback_data' => "confirm_delete_server_$serverId"],
-                        ['text' => "❌ Нет", 'callback_data' => "no_delete_server_$serverId"],
+                        ['text' => "❌ Нет", 'callback_data' => "cancel_delete_server_$serverId"],
                     ]
                 ]);
                 $bot->editMessageText($chat_id, $message->getMessageId(), 'Вы точно хотите удалить сервер? ' . PHP_EOL . $serverName . PHP_EOL . 'ID: ' . $serverId, null, false, $keyboard);
-                //handleServerDeleteRequest($serverId, TOKEN_REG_RU, URL);
+                handleServerDeleteRequest($serverId, TOKEN_REG_RU, URL);
                 break;
             case (bool)preg_match('/^confirm_delete_server_(\d+)$/', $callback_data, $matches):
                 $serverId = $matches[1];
@@ -112,7 +112,7 @@ try {
                 $bot->editMessageText($chat_id, $message->getMessageId(), 'Удаляем сервер: ' . PHP_EOL . $serverName . PHP_EOL . 'ID: ' . $serverId, null, false, $keyboard);
                 //handleServerDeleteRequest($serverId, TOKEN_REG_RU, URL);
                 break;
-            case (bool)preg_match('/^no_delete_server_(\d+)$/', $callback_data, $matches):
+            case (bool)preg_match('/^cancel_delete_server_(\d+)$/', $callback_data, $matches):
                 $serverId = $matches[1];
                 $serverList = handleServerListRequest(TOKEN_REG_RU, URL);
                 // Извлекаем информацию о конкретном сервере
@@ -128,7 +128,24 @@ try {
                 // Отправляем информацию только о выбранном сервере
                 $bot->editMessageText($chat_id, $message->getMessageId(), reset($serverInfo), null, false, $keyboard);
                 break;
+            case (bool)preg_match('/^confirm_reload_server_(\d+)$/', $callback_data, $matches):
+                $serverId = $matches[1];
+                // Получаем информацию о серверах
+                $serverList = handleServerListRequest(TOKEN_REG_RU, URL);
+                // Извлекаем информацию о конкретном сервере
+                $serverInfo = preg_grep("/ID: $serverId/", explode("\n\n", $serverList));
+                // Извлекаем имя сервера
+                preg_match('/Имя сервера: (.+)/', reset($serverInfo), $names);
+                $serverName = $names[1] ?? 'не найден';
 
+                $keyboard = new InlineKeyboardMarkup([
+                    [
+                        ['text' => "Все сервера", 'callback_data' => 'all_servers']
+                    ]
+                ]);
+                $bot->editMessageText($chat_id, $message->getMessageId(), 'Начинаем перезагружать сервер: ' . PHP_EOL . $serverName . PHP_EOL . 'ID: ' . $serverId, null, false, $keyboard);
+                handleServerReboot($serverId, TOKEN_REG_RU, URL);
+                break;
 
             default:
                 $bot->sendMessage($chat_id, 'Неизвестная опция' . $callback_data);
